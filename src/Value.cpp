c42ec47a1252dd3f075acc12d7400a9f74f11da9
Turaiiao <3106740988@qq.com>
2019-06-18 08:09:27 +0800

    update

---
+++ src/Value.cpp
@@ -0,0 +1,39 @@
+/**
+ * Meet Programming Language Values.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) 2019 Turaiiao
+ * Email: 1171840237@qq.com
+ * Github: https://github.com/turaiiao
+ */
+#include "Value.hpp"
+
+Value::Value(double value) {
+    this->valueNumber = true;
+    this->numberValue = std::move(value);
+}
+
+Value::Value(std::string value) {
+    this->valueString = true;
+    this->stringValue = std::move(value);
+}
+
+Value::Value(bool value) {
+    this->valueBool = true;
+    this->boolValue = value;
+}
+
+Value::setConstant(bool constant) {
+    this->valueConstant = constant;
+}
