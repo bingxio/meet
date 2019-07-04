a1d7ac786864b5de98a9f412df61e8e51174f60c
Turaiiao <3106740988@qq.com>
2019-07-04 15:15:15 +0800

    update

--- src/expressions/LogicalExpression.hpp
+++ src/expressions/LogicalExpression.hpp
@@ -21,7 +21,7 @@
 #define MEET_LOGICAL_EXPRESSION_H
 
 #include "../Token.hpp"
-##include "../interpreter/Expression.hpp"
+#include "../interpreter/Expression.hpp"
 
 class LogicalExpression: public Expression {
     public:
